1f0a43390b69ca4ecfe8f21af282cc492f064675
bruno <bruno@workstation.desafiodesign>
2021-09-13 20:16:44 -0300

    Foi criado o controller AuthController para criado o metodo de registro

--- back-end/app/Http/Controllers/AuthController.php
+++ back-end/app/Http/Controllers/AuthController.php
@@ -3,8 +3,62 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+    public function unauthorized() 
+    {
+        return response()->json([
+            'error'=>'Não autorizado'
+        ],401);
+    }
+
+    public function register(Request $request)
+    {
+        $array = ['error'=>''];
+
+        $validator = Validator::make($request->all(),[
+            'name'=>'required',
+            'email'=>'required|email|unique:users,email',
+            'phone'=>'required',
+            'password'=>'required|min:6',
+            'address_complement'=>'required'
+        ]);
+
+        if ($validator->fails()) {
+            $array['error'] = $validator->errors()->first();
+            return $array;
+        }
+
+        $name = $request->input('name');
+        $email = $request->input('email');
+        $phone = $request->input('phone');
+        $password = $request->input('password');
+        $addressComplement = $request->input('address_complement');
+        
+        $hashPassword = password_hash($password,PASSWORD_BCRYPT);
+
+        $user = new User();
+        $user->name = $name;
+        $user->email = $email;
+        $user->phone = $phone;
+        $user->password = $hashPassword;
+        $user->address_complement = $addressComplement;
+        $user->save();
+
+        $token = Auth::attempt(['email' => $email, 'password' => $password]);
+
+        if (!$token) {
+            $array['error'] = 'Ocorreu um erro';
+            return $array;
+        }
+
+        $array['token'] = $token;
+        $array['user'] = auth::user();
+
+        return $array;
+    }
 }
--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/401',[AuthController::class,'unautrorized'])->name('login');
+Route::get('/401',[AuthController::class,'unauthorized'])->name('login');
 
 Route::post('/auth/login',[AuthController::class,'login']);
 Route::post('/auth/register',[AuthController::class,'register']);
